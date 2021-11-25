3acf2dc61ea2e6a5463e1a5fb1913d67d2b5b989
Philipp Piwo <github@piwo.space>
2021-11-25 17:19:53 +0100

    example remote call events decode

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-const {Universal, Node, MemoryAccount, Crypto} = require('@aeternity/aepp-sdk')
+const {Universal, Node, MemoryAccount} = require('@aeternity/aepp-sdk')
 
 const exampleSource = `
 contract Example =
-  entrypoint example(x : int) = x
+  datatype event = E_A(address, int)
+
+  stateful entrypoint example(x : int) = 
+    Chain.event(E_A(Call.caller, x))
+    x
 `
 
-const factorySource = `
-contract Example =
-  entrypoint example(x : int) = x
+const remoteCallSource = `
+contract interface Example =
+  stateful entrypoint example : (int) => int 
 
-main contract Factory =
-  stateful entrypoint create() =
-    Chain.create() : Example
+main contract RemoteCall =
+  datatype event = E_B(address, int)
+  stateful entrypoint call_remote(example : Example, x : int) =
+    Chain.event(E_B(Call.caller, x))
+    example.example(x)
 `
 
 const main = async () => {
-  //const keypair = Crypto.generateKeyPair()
-  //console.log('keypair', keypair)
   const keypair = {
     publicKey: 'ak_4FJFaSFiTts7uStAWa7wXSHxXXrueH2f7XREykDCL21wDBxpM',
     secretKey: 'e8fc19fae94a754bcb9233b1d95d9740828a7af4ead066c3af01fcbb80202bb8075f20af877e2d8e915a4851d1791675a08e26bc91bbb88ea86eaa2384c84322'
   }
 
   const client = await Universal({
-    compilerUrl: 'https://latest.compiler.aepps.com',
+    compilerUrl: 'http://localhost:3080/',
     nodes: [{
       name: 'node',
-      instance: await Node({url: 'http://localhost:3013/'}),
+      instance: await Node({url: 'http://localhost:3001/', ignoreVersion: true}),
     }],
     accounts: [
       MemoryAccount({keypair}),
     ],
   })
 
-  const factoryContract = await client.getContractInstance(factorySource)
-  await factoryContract.deploy()
-  console.log('deployed factory')
+  const exampleContract = await client.getContractInstance({source: exampleSource})
+  await exampleContract.deploy()
+  console.log('deployed example', exampleContract.deployInfo.address)
 
-  const exampleContractAddress = (await factoryContract.methods.create()).decodedResult
-  console.log('created example', exampleContractAddress)
+  const remoteCallContract = await client.getContractInstance({source: remoteCallSource})
+  await remoteCallContract.deploy()
+  console.log('deployed remote caller')
 
-  const exampleContract = await client.getContractInstance(exampleSource, {contractAddress: exampleContractAddress})
-  console.log('called example', (await exampleContract.methods.example(42)).decodedResult)
+
+  const callRemote = await remoteCallContract.methods.call_remote(exampleContract.deployInfo.address, 42)
+  console.log('called remote events', callRemote.decodedEvents)
+  console.log('remote events', exampleContract.decodeEvents(callRemote.result.log))
 }
 
 main()
